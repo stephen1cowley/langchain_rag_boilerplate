da5ab3d6a3ac74caac07fe17e8f2187aad82fe3f
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-06-20 16:58:08 +0100

    workign server

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ from langchain_chroma import Chroma
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.prebuilt import create_react_agent
+from langchain_core.messages import AIMessage, HumanMessage
 import dotenv
 from langgraph.checkpoint import MemorySaver
 
+
 dotenv.load_dotenv()
-memory = SqliteSaver.from_conn_string(":memory:")
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
 vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=OpenAIEmbeddings())
@@ -19,29 +20,21 @@ retriever = vectorstore.as_retriever()
 ### Build retriever tool ###
 tool = create_retriever_tool(
     retriever,
-    "blog_post_retriever",
-    "Searches and returns excerpts from the Autonomous Agents blog post.",
+    "case_law",
+    "Searches and returns excerpts from 2023 UK case law.",
 )
 tools = [tool]
 
 system_message = "You are a helpful assistant with access to a tool."
 agent_executor = create_react_agent(llm, tools, checkpointer=MemorySaver(), messages_modifier=system_message)
 
+### Make the query ###
+# query = "What does the potentiality of AI agents depend on?"
+query = "What's the weather like today?"
+config = {"configurable": {"thread_id": "abc124"}}
 
-app = FastAPI(
-  title="LangChain Server",
-  version="1.0",
-  description="A simple API server using LangChain's Runnable interfaces",
-)
-
-
-add_routes(
-    app,
-    agent_executor,
-    path="/my_runnable",
-)
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="localhost", port=8000)
+for s in agent_executor.stream(
+    {"messages": [HumanMessage(content=query)]}, stream_mode="updates", config=config
+):
+    print(s)
+    print("----")
--- db_query.py
+++ db_query.py
@@ -9,6 +9,8 @@ query = "Removal of asylum seekers to Rwanda"
 
 db = Chroma(persist_directory="./chroma_db", embedding_function=OpenAIEmbeddings())
 docs = db.similarity_search(query)
+
+
 print(docs[0].page_content)
 print(docs[0].metadata)
 
