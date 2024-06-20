32ddce214c4c619c0c299375eaa54379092448f4
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-06-20 12:31:19 +0100

    Web scraper unique links

--- web_scraper.py
+++ web_scraper.py
@@ -4,8 +4,17 @@ from bs4 import BeautifulSoup
 # The URL of the parent page
 url = "https://www.bailii.org/ew/cases/EWHC/Admin/2023/"
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Referer": "https://example.com/",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+}
+
+
 # Send a request to fetch the HTML content of the parent page
-response = requests.get(url)
+response = requests.get(url, headers=headers)
 html_content = response.content
 
 # Parse the HTML content using BeautifulSoup
@@ -19,13 +28,17 @@ links = [tag.get('href') for tag in a_tags if tag.get('href')]
 
 # Filter the links if needed (e.g., remove links to external sites, duplicates, etc.)
 # Here we assume that subpages are relative URLs (not starting with http or https)
-subpage_links = [link for link in links if not link.startswith(('http://', 'https://'))]
+subpage_links = [link for link in links if link.startswith(('/ew/cases/EWHC/Admin/2023/'))]
+unique_links = []
+for link in subpage_links:
+    if link not in unique_links:
+        unique_links.append(link)
 
 # Count the number of subpages
-num_subpages = len(subpage_links)
+num_subpages = len(unique_links)
 
 print(f"Number of possible HTML subpages: {num_subpages}")
 
 # Optionally, print the list of subpage links
-for link in subpage_links:
+for link in unique_links:
     print(link)
